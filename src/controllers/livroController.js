74526718f5ad2a62fe41fec383680eaa4747f6e0
pedro felipe <pedrofelipinho@gmail.com>
2024-07-17 15:36:31 -0300

    Colocando o get by livro e o put no controller e já adicionando as rotas

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,22 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listarLivros (req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na requisição` })
+        }
+    };
+
+    static async listarLivroPorId (req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na requisição do livro` })
+        }
     };
 
     static async cadastrarLivro (req, res) {
@@ -14,7 +28,17 @@ class LivroController {
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` })
         }
-    }
+    };
+
+    static async atualizarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Livro atualizado com sucesso!" });
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na atualização` })
+        }
+    };
 };
 
 export default LivroController;
