7c6bb1b2f51ee535f9089851d83e951ef410de33
pedro felipe <pedrofelipinho@gmail.com>
2024-07-17 17:32:28 -0300

    Colocando o delete no Controller

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -39,6 +39,16 @@ class LivroController {
             res.status(500).json({ message: `${erro.message} - falha na atualização` })
         }
     };
+
+    static async excluirLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({ message: 'Livro removido com sucesso' });
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha para apagar` })
+        }
+    }
 };
 
 export default LivroController;
